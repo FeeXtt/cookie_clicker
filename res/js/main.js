f3d1152083141c7e65ad44c59445efbdce2f7920
Stanislav Cinkl <stanislav.cinkl@student.spsmb.cz>
2023-09-27 10:34:16 +0200

    upgrade

--- res/js/main.js
+++ res/js/main.js
@@ -8,10 +8,14 @@ Cheaty
 Animace na kliknutí
 */
 
+
 const cookie = document.getElementById("cookie");
 const counter = document.getElementById("counter");
+const upgrade = document.getElementById("upgradeClickButton") 
+
 
 let numberOfCookies = 0;
+let clickIncrease = 1;
 
 // {} - scope
 cookie.onclick = () => {
@@ -19,7 +23,20 @@ cookie.onclick = () => {
     //numberOfCookies = numberOfCookies + 1;
     //numberOfCookies += 1;
     // ++ inkrement
-    numberOfCookies++;
+    numberOfCookies += clickIncrease;
     console.log(numberOfCookies);
     counter.innerText = "Cookies: " + numberOfCookies;
+}
+// () => scope
+upgrade.onclick = () => { 
+    if (numberOfCookies >= 50) {
+        //odečtu sušenky
+        numberOfCookies -= 50;
+        //numberOfCookies = numberOfCookies - 50
+        counter.innerText = "Cookies: " + numberOfCookies;
+        clickIncrease++;
+       
+
+    }
+
 }
