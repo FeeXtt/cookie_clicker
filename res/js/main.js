36f467462330deda8da41170b0ec4aeef5e6f9fc
Stanislav Cinkl <stanislav.cinkl@student.spsmb.cz>
2023-10-04 10:04:39 +0200

    autoclick

--- res/js/main.js
+++ res/js/main.js
@@ -8,35 +8,62 @@ Cheaty
 Animace na kliknutí
 */
 
-
+/*
+constanta = proměná
+proměná - vymezené místo v paměti počítače, dá se upravit, smazat a číst
+*/
 const cookie = document.getElementById("cookie");
 const counter = document.getElementById("counter");
-const upgrade = document.getElementById("upgradeClickButton") 
-
+const upgrade = document.getElementById("upgradeClickButton");
+const autoclick = document.getElementById("autoClickButton");
 
 let numberOfCookies = 0;
 let clickIncrease = 1;
+let autoclickIncrease = 0;
+let autoclicker = 1;
+let autoclickerPrice = 100;
 
-// {} - scope
+// {} - scope - definice funkce
 cookie.onclick = () => {
-    console.log("click");
-    //numberOfCookies = numberOfCookies + 1;
-    //numberOfCookies += 1;
-    // ++ inkrement
-    numberOfCookies += clickIncrease;
-    console.log(numberOfCookies);
-    counter.innerText = "Cookies: " + numberOfCookies;
-}
+  // => arrow funkce
+  console.log("click"); // do konzole vypíše click
+  //numberOfCookies = numberOfCookies + 1;
+  //numberOfCookies += 1;
+  // ++ inkrement
+  numberOfCookies += clickIncrease;
+  console.log(numberOfCookies);
+  counter.innerText = "Cookies: " + numberOfCookies;
+};
 // () => scope
-upgrade.onclick = () => { 
-    if (numberOfCookies >= 50) {
-        //odečtu sušenky
-        numberOfCookies -= 50;
-        //numberOfCookies = numberOfCookies - 50
-        counter.innerText = "Cookies: " + numberOfCookies;
-        clickIncrease++;
-       
+upgrade.onclick = () => {
+  if (numberOfCookies >= 50) {
+    //odečtu sušenky
+    numberOfCookies -= 50;
+    //numberOfCookies = numberOfCookies - 50
+    counter.innerText = "Cookies: " + numberOfCookies;
+    clickIncrease++;
+  }
+};
+autoclick.onclick = () => {
+  if (numberOfCookies >= autoclickerPrice) {
+    // odectu 100
+    numberOfCookies -= autoclickerPrice;
+    
+    //aktualizuju odstavec;
+    counter.innerText = "Cookies: " + numberOfCookies;
+    autoclickerPrice  += 100;
+    autoclick.innerText = "Buy auto click" + autoclickerPrice;
+    autoclickIncrease++;
+    // interval - opakování po case pridani susenky
+    clearInterval(autoclicker);
+    autoclicker = setInterval(() => {
+      numberOfCookies += autoclickIncrease;
+      counter.innerText = "Cookies: " + numberOfCookies;
+    }, 1000);
+  }
+};
 
-    }
-
+const turnOnCheats = () => {
+    numberOfCookies += 100000000;
+    counter.innerText = "Cookies: " + numberOfCookies;
 }
